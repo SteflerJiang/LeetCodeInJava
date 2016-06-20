c8ca044a1a187246109af9e9f95b0ece0c562886
Stefler Jiang <steflerjiang@163.com>
2016-06-20 21:04:07 +0800

    add java doc

---
+++ src/com/stefler/_344_ReverseString.java
@@ -0,0 +1,18 @@
+package com.stefler;
+
+public class _344_ReverseString {
+	public static String reverseString(String s) {
+		StringBuilder sb = new StringBuilder(s.length());
+		for (int i = s.length() - 1; i >= 0; i--)
+		{
+			sb.append(s.charAt(i));
+		}
+		return sb.toString();
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println(reverseString("hello"));
+	}
+
+}
