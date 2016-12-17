3ebbca487c8676252ab6f9c80479a789d1a880f8
qinjiu <qinjiu@2dfire.com>
2016-12-17 19:39:17 +0800

    add 98

--- src/com/stefler/_104_MaximumDepthofBinaryTree.java
+++ src/com/stefler/_104_MaximumDepthofBinaryTree.java
@@ -11,6 +11,11 @@ class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
+    TreeNode(int x, TreeNode left, TreeNode right) {
+        this.val = x;
+        this.left = left;
+        this.right = right;
+    }
 }
 
 public class _104_MaximumDepthofBinaryTree {
