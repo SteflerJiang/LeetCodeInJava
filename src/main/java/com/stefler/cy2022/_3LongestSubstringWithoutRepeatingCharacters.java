ef00e302ad9a2aad10bf4024ff7eec871f9495a2
shizheng.jsz <steflerjiang@163.com>
2022-05-29 20:57:35 +0800

    add by 2022.05.29

--- src/main/java/com/stefler/cy2022/_3LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/com/stefler/cy2022/_3LongestSubstringWithoutRepeatingCharacters.java
@@ -1,12 +1,24 @@
 package com.stefler.cy2022;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class _3LongestSubstringWithoutRepeatingCharacters {
     public static int lengthOfLongestSubstring(String s) {
-        return 0;
+        int res = 0;
+        int start = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            start = Integer.max(start, map.getOrDefault(c, -1) + 1);
+            res = Integer.max(res, i - start + 1);
+            map.put(c, i);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring(""));
+        System.out.println(lengthOfLongestSubstring("bbbbbb"));
     }
 
 }
---
+++ src/main/java/com/stefler/cy2022/_4MedianOfTwoSortedArrays.java
@@ -0,0 +1,12 @@
+package com.stefler.cy2022;
+
+public class _4MedianOfTwoSortedArrays {
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(findMedianSortedArrays(new int[]{1, 2}, new int[]{1, 2}));
+    }
+
+}
